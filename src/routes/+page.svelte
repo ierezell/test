51961851430eaa2826ddb74c5c831c81631c62ee
Pierre Snell <psnell@biotwin.ai>
2023-07-24 11:15:11 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import welcome_fallback from "$lib/images/svelte-welcome.png";
+	import welcome from "$lib/images/svelte-welcome.webp";
+	import Counter from "./Counter.svelte";
+
+	export let data;
+</script>
+
+<svelte:head>
+	<title>User Management</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+<div class="row flex-center flex">
+	<div class="col-6 form-widget">
+		<Auth
+			supabaseClient={data.supabase}
+			view="magic_link"
+			redirectTo={`${data.url}/auth/callback`}
+			showLinks={false}
+			appearance={{ theme: ThemeSupa, style: { input: "color: #fff" } }}
+		/>
+	</div>
+</div>
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
